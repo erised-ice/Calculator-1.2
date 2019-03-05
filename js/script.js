c4e511b6782cb6a303fbe0b1bef12e8fa033efff
alexandra <le.onze.mai@gmail.com>
2019-03-06 00:57:14 +0300

    [calculator]code refactoring

--- js/script.js
+++ js/script.js
@@ -14,36 +14,38 @@ function stringToNumber(string) {
 const $outputDisplay = document.querySelector('.js-output');
 const $buttonCollection = document.querySelectorAll('.js-button');
 
+function calculate(event) {
+  const button = event.target;
+  const buttonValue = button.dataset.operation;
+  const inputValueOne = getInputValue('first-num');
+  const inputValueTwo = getInputValue('second-num');
+  const numberOne = stringToNumber(inputValueOne);
+  const numberTwo = stringToNumber(inputValueTwo);
+
+  let result = null;
+
+  switch (buttonValue) {
+    case 'addition':
+      result = numberOne + numberTwo;
+      break;
+    case 'subtraction':
+      result = numberOne - numberTwo;
+      break;
+    case 'multiply':
+      result = numberOne * numberTwo;
+      break;
+    case 'division':
+      result = numberOne / numberTwo;
+      break;
+    case 'reset':
+      document.getElementById('first-num').value = '';
+      document.getElementById('second-num').value = '';
+      result = '';
+  }
+
+  output();
+}
+
 $buttonCollection.forEach(function (button) {
-  button.addEventListener('click', function () {
-    const buttonValue = button.value;
-    var inputValueOne = getInputValue('first-num');
-    var inputValueTwo = getInputValue('second-num');
-
-    let numberOne = stringToNumber(inputValueOne);
-    let numberTwo = stringToNumber(inputValueTwo);
-
-    let result = null;
-
-    switch (buttonValue) {
-      case 'addition':
-        result = numberOne + numberTwo;
-        break;
-      case 'subtraction':
-        result = numberOne - numberTwo;
-        break;
-      case 'multiply':
-        result = numberOne * numberTwo;
-        break;
-      case 'division':
-        result = numberOne / numberTwo;
-        break;
-      case 'reset':
-        document.getElementById('first-num').value = '';
-        document.getElementById('second-num').value = '';
-        result = '';
-    }
-
-    output(result);
-  });
+  button.addEventListener('click', calculate);
 });
